9195e6e9a51762d9298971d0dde3f401b46d3ae1
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-08-10 23:12:17 -0300

    feat(app): Adicionar componente Book ao layout principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./components/header/header";
 import { Footer } from './components/footer/footer';
+import { Book } from './components/book/book';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer],
+  imports: [Header, Footer, Book],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
