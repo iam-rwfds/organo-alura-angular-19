8c411a873222824c2e3a9c013af2a393c8729bf8
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-08-13 11:57:01 -0300

    refactor(app): remover LiteraryGenre do AppComponent
    Remove a importação e o uso do componente LiteraryGenre do AppComponent.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { BookList } from './components/book-list/book-list';
-import { LiteraryGenre } from './components/literary-genre/literary-genre';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, BookList, LiteraryGenre],
+  imports: [Header, Footer, BookList],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
