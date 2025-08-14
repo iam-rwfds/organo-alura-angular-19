89996cb4810e692e8d3768e5eae28409af079c37
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-08-14 10:03:28 -0300

    feat(book): Habilitar borda colorida por gênero em livros
    Adiciona CommonModule e usa [ngClass] para aplicar a classe de estilo baseada no ID do gênero.

--- src/app/components/book/book.ts
+++ src/app/components/book/book.ts
@@ -1,8 +1,10 @@
 import { Component, input } from '@angular/core';
 import { BookType } from './book.entity';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book',
+  imports: [CommonModule],
   templateUrl: './book.html',
   styleUrl: './book.scss',
 })
