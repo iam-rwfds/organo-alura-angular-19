97d8133d8b60b933c36659a7eb1d0c574252462d
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-08-14 10:04:59 -0300

    feat(genero-literario): Adicionar estado vazio e aplicar estilos por ID

--- src/app/components/literary-genre/literary-genre.ts
+++ src/app/components/literary-genre/literary-genre.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Book } from '../book/book';
 import { LiteraryGenreType } from '../book/book.entity';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-literary-genre',
-  imports: [Book],
+  imports: [Book, CommonModule],
   templateUrl: './literary-genre.html',
   styleUrl: './literary-genre.scss',
 })
